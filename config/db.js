784ed7ab22ce258b2690cd80116bffead83db0b3
sharavanakumar2 <sharavanakumar964@gmail.com>
2025-02-26 07:55:55 +0530

    Update MONGODB_URI

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/recipeDB'); // Removed options
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+    const connectDB = async () => {
+      try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB Connected...');
+      } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+    };
+    module.exports = connectDB;
